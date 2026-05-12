89a179b8c3164355827ffea7a0d3bf52d61f5442
voidptrr <bruno.tommaso@protonmail.com>
2026-05-12 18:29:24 +0100

    ci: improve checks and cmake config

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -4,17 +4,18 @@
     config,
     ...
   }: {
-    packages = import ./packages {inherit pkgs;};
+      packages = import ./packages {inherit pkgs;};
 
     formatter = pkgs.alejandra;
 
-    devShells.default = pkgs.mkShell {
-      packages = [
-        pkgs.gcc
-        config.packages.format-all
-        config.packages.format-check
-        config.packages.test-all
-      ];
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.gcc
+          config.packages.format-all
+          config.packages.format-check
+          config.packages.test-all
+          config.packages.code-check
+        ];
     };
   };
 }
